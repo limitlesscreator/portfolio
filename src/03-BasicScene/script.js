8257a66d0e95f009feea37931076dce7f48127ab
Vladislav V <0428vlad0428@mail.ru>
2021-09-15 16:20:52 +0300

    practice with transform objects :) so cool, so nice

--- src/03-BasicScene/script.js
+++ src/03-BasicScene/script.js
@@ -1,14 +1,43 @@
 // import React from 'react';
+import s from '../style.sass'
 import * as THREE from 'three'
 
 // Scene
 const scene = new THREE.Scene()
 
-// Purple cube
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({color: 'hsl(270, 50%, 50%)'})
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+// Objects
+const group = new THREE.Group()
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 'blue'})
+)
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 'hsl(800, 80%, 50%)'})
+)
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 'red'})
+)
+cube2.position.x = -2
+cube3.position.x = 2
+
+// cube1.position.z = -1
+// cube2.position.z = -1
+// cube3.position.z = -1
+
+group.add(cube1,cube2,cube3)
+group.position.y = 1
+group.scale.y = 2
+group.rotation.y = 1
+
+// Axes helper
+const axesHelper = new THREE.AxesHelper(2)
+// scene.add(axesHelper)
+scene.add(axesHelper)
+
 
 //Sized
 const sizes = {
@@ -19,8 +48,35 @@ const sizes = {
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
-camera.position.x = 1
+camera.position.x = 0
+camera.position.y = 1
 scene.add(camera)
+// camera.lookAt(mesh.position)
+
+// console.log(mesh.position.distanceTo(camera.position))
+// console.log(mesh.position.normalize())
+// console.log(mesh.position.length())
+
+// // Purple cube
+// const geometry = new THREE.BoxGeometry(1, 1, 1)
+// const material = new THREE.MeshBasicMaterial({color: 'hsl(270, 50%, 50%)'})
+// const mesh = new THREE.Mesh(geometry, material)
+// scene.add(mesh)
+//
+//
+// // Scale
+// mesh.scale.set(2,0.5,0.5)
+//
+// // Rotation
+// mesh.rotation.set(Math.PI * 0.25,Math.PI * 0.25,0)
+//
+// //Position
+// // mesh.position.x = 0.7
+// // mesh.position.y = -0.6
+// // mesh.position.z = 1
+// mesh.position.set(0.7, -0.6, 1)
+
+
 
 //Renderer
 const canvas = document.querySelector('.webgl')
@@ -29,5 +85,4 @@ const renderer = new THREE.WebGLRenderer({
     canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-
 renderer.render(scene, camera)
