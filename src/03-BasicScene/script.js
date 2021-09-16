6ffc10d99611bfc618771f3ca08639223cece4ca
Vladislav V <0428vlad0428@mail.ru>
2021-09-16 19:59:57 +0300

    some glitches, but i'll be fix that

--- src/03-BasicScene/script.js
+++ src/03-BasicScene/script.js
@@ -1,45 +1,24 @@
 // import React from 'react';
 import s from '../style.sass'
 import * as THREE from 'three'
+import gsap from 'gsap'
+
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
 
-// Objects
-const group = new THREE.Group()
-scene.add(group)
-
-const cube1 = new THREE.Mesh(
-    new THREE.BoxGeometry(1,1,1),
-    new THREE.MeshBasicMaterial({color: 'blue'})
-)
-const cube2 = new THREE.Mesh(
-    new THREE.BoxGeometry(1,1,1),
-    new THREE.MeshBasicMaterial({color: 'hsl(800, 80%, 50%)'})
-)
-const cube3 = new THREE.Mesh(
-    new THREE.BoxGeometry(1,1,1),
-    new THREE.MeshBasicMaterial({color: 'red'})
-)
-cube2.position.x = -2
-cube3.position.x = 2
-
-// cube1.position.z = -1
-// cube2.position.z = -1
-// cube3.position.z = -1
-
-group.add(cube1,cube2,cube3)
-group.position.y = 1
-group.scale.y = 2
-group.rotation.y = 1
-
-// Axes helper
-const axesHelper = new THREE.AxesHelper(2)
-// scene.add(axesHelper)
-scene.add(axesHelper)
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
 
 
-//Sized
+
+// Sizes
 const sizes = {
     width: 800,
     height: 600
@@ -48,41 +27,44 @@ const sizes = {
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
-camera.position.x = 0
-camera.position.y = 1
 scene.add(camera)
-// camera.lookAt(mesh.position)
 
-// console.log(mesh.position.distanceTo(camera.position))
-// console.log(mesh.position.normalize())
-// console.log(mesh.position.length())
-
-// // Purple cube
-// const geometry = new THREE.BoxGeometry(1, 1, 1)
-// const material = new THREE.MeshBasicMaterial({color: 'hsl(270, 50%, 50%)'})
-// const mesh = new THREE.Mesh(geometry, material)
-// scene.add(mesh)
-//
-//
-// // Scale
-// mesh.scale.set(2,0.5,0.5)
-//
-// // Rotation
-// mesh.rotation.set(Math.PI * 0.25,Math.PI * 0.25,0)
-//
-// //Position
-// // mesh.position.x = 0.7
-// // mesh.position.y = -0.6
-// // mesh.position.z = 1
-// mesh.position.set(0.7, -0.6, 1)
-
-
-
-//Renderer
-const canvas = document.querySelector('.webgl')
+// Renderer
 const renderer = new THREE.WebGLRenderer({
-    // canvas: canvas
-    canvas
+    canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
+
+// Clock
+const clock = new THREE.Clock()
+
+// Animations
+gsap.to(mesh.position, {duration: 1, delay: 1, x: 1})
+gsap.to(mesh.position, {duration: 1, delay: 2, y: 1})
+gsap.to(mesh.position, {duration: 1, delay: 3, x: 0})
+gsap.to(mesh.position, {duration: 1, delay: 4, y: 0})
+
+const tick = () => {
+    // Clock
+    // const elapsedTime = clock.getElapsedTime()
+
+    // Update objects
+    // mesh.position.y = Math.sin(elapsedTime)
+    // mesh.position.x = Math.cos(elapsedTime)
+    // mesh.rotation.y = elapsedTime
+    // mesh.scale.y += 0.001 * deltaTime
+
+    // camera.lookAt(mesh.position)
+
+    //Render
+    renderer.render(scene, camera)
+    window.requestAnimationFrame(tick)
+}
+
+tick()
+
+
+
+
+
--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import {Script0} from "./00-MyScene/script0";
-import {Script} from "./03-BasicScene/script";
-
+import React from 'react';
 
 function App() {
 
     return (
         <div>
-            {/*<canvas className={'webgl'}>*/}
-            {/*</canvas>*/}
-            {/*<Script/>*/}
-        </div>
 
+        </div>
     );
 }
 
